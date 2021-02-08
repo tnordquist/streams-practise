741c35e907932fd0315911ba769e62bcdacf6424
tnordquist <toddnordquist144@gmail.com>
2021-02-07 21:40:12 -0700

    Stream skip()

--- src/main/java/edu/cnm/hsd/java8streams/App.java
+++ src/main/java/edu/cnm/hsd/java8streams/App.java
@@ -1,32 +1,20 @@
 package edu.cnm.hsd.java8streams;
 
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-	private static final String[] colors = {"Red", "Red", "Orange", "Orange", "Yello", "Green"}; {
-		
-	}
-    public static void main( String[] args ) {
-       
-//    	long count = Arrays.stream(colors).count();
-    	
-    	long count = Arrays.asList(colors).stream()
-    			.limit(2)
-    			.count();
-    	
-    	Arrays.asList(colors).stream()
-    			.limit(2)
-    			.forEach(System.out::println);
-    	
-//    	long count = Stream.of(colors).count();
-    	
-    	System.out.println(count);
-    	
-    }
+public class App {
+  private static final String[] colors =
+      {"Red", "Red", "Orange", "Orange", "Yello", "Green", "Blue", "Indigo", "Violet"};
+
+
+  public static void main(String[] args) {
+
+    // skip
+    Arrays.asList(colors).stream().skip(4).forEach(System.out::println);
+
+  }
 }
