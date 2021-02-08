596a6d6f4c9191af6f66d136d86ee4dd102c5336
tnordquist <toddnordquist144@gmail.com>
2021-02-07 21:20:28 -0700

    Intermediate: limit

--- src/main/java/edu/cnm/hsd/java8streams/App.java
+++ src/main/java/edu/cnm/hsd/java8streams/App.java
@@ -17,11 +17,11 @@ public class App
 //    	long count = Arrays.stream(colors).count();
     	
     	long count = Arrays.asList(colors).stream()
-    			.distinct()
+    			.limit(2)
     			.count();
     	
     	Arrays.asList(colors).stream()
-    			.distinct()
+    			.limit(2)
     			.forEach(System.out::println);
     	
 //    	long count = Stream.of(colors).count();
