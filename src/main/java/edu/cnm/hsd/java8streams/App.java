1a26e07b9a2eb7d2c16c0af2dd3b34a0cbd91ad5
tnordquist <toddnordquist144@gmail.com>
2021-02-12 19:45:35 -0700

    Predicate, first look.

--- src/main/java/edu/cnm/hsd/java8streams/App.java
+++ src/main/java/edu/cnm/hsd/java8streams/App.java
@@ -1,6 +1,7 @@
 package edu.cnm.hsd.java8streams;
 
 import java.util.Arrays;
+import java.util.function.Predicate;
 
 /**
  * Hello world!
@@ -12,10 +13,12 @@ public class App {
 
   private static final Integer[] nums = {34, 67, 9, 12, -3};
 
-
   public static void main(String[] args) {
 
-    // sorted
-    Arrays.asList(nums).stream().distinct().sorted().forEach(System.out::println);
+    // Predicate
+    long count = Arrays.asList(colors).stream().filter(isLengthThreeChars).count();
+    System.out.println(count);
   }
+
+  private static Predicate<String> isLengthThreeChars = color -> color.length() == 6;
 }
