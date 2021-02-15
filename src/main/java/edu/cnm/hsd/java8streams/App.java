cc3e3b51e0620e04059f7f1bf54e212857818f2a
tnordquist <toddnordquist144@gmail.com>
2021-02-14 20:56:05 -0700

    Compare method: functional interface.

--- src/main/java/edu/cnm/hsd/java8streams/App.java
+++ src/main/java/edu/cnm/hsd/java8streams/App.java
@@ -1,12 +1,13 @@
 package edu.cnm.hsd.java8streams;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Optional;
 
 public class App {
-  public static final String[] colors =
-      {"Red", "Red", "Orange", "Orange", "Yello", "Green", "Blue", "Indigo", "Violet"};
+  /*
+   * public static final String[] colors = {"Red", "Red", "Orange", "Orange", "Yello", "Green",
+   * "Blue", "Indigo", "Violet"};
+   */
 
   private static final Integer[] nums = {34, 67, 9, 12, -3};
   private static final int[] nums2 = {34, 67, 9, 12, -3, 1001};
@@ -14,12 +15,14 @@ public class App {
   public static void main(String[] args) {
     Optional<User> max = Arrays
         .asList(new User("Tony", 45, Arrays.asList("Red", "Green")),
-            new User("Mark", 21, Arrays.asList("Blue", "Green", "White")),
-            new User("Ben", 19, Arrays.asList("Red", "Green", "Yellow", "Indigo")),
-            new User("Jed", 61, Arrays.asList("Blue", "Green", "White", "Orange", "Fuscia")))
-        .stream().max(Comparator.comparing(User::getAge));
+            new User("Mark", 61, Arrays.asList("Blue", "Green", "White")),
+            new User("Adam", 61,
+                Arrays.asList("Blue", "Green", "White", "Orange", "Fuscia", "Ink")),
+            new User("Ben", 19, Arrays.asList("Blue", "Green", "White", "Orange", "Fuscia", "Ink")))
+        .stream().max((val1, val2) -> val1.getColors().size() < val2.getColors().size() ? -1
+            : val1.getColors().size() > val2.getColors().size() ? 1 : 0);
 
-    max.ifPresent(maxVal -> System.out.println(maxVal.getFirstName()));
+    max.ifPresent(val -> System.out.println(val.getFirstName()));
   }
 
   public static long aConsumer(String[] colors) {
