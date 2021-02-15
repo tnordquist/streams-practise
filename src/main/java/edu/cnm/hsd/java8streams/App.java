c7efcba30a5dfa7be7c93f7f41bc16fea0def1f8
tnordquist <toddnordquist144@gmail.com>
2021-02-14 20:32:04 -0700

    Comparator comparing.

--- src/main/java/edu/cnm/hsd/java8streams/App.java
+++ src/main/java/edu/cnm/hsd/java8streams/App.java
@@ -1,8 +1,8 @@
 package edu.cnm.hsd.java8streams;
 
 import java.util.Arrays;
-import java.util.OptionalInt;
-import java.util.stream.IntStream;
+import java.util.Comparator;
+import java.util.Optional;
 
 public class App {
   public static final String[] colors =
@@ -12,11 +12,14 @@ public class App {
   private static final int[] nums2 = {34, 67, 9, 12, -3, 1001};
 
   public static void main(String[] args) {
-    OptionalInt max = Arrays.asList(nums).stream().mapToInt(value -> value).max();
+    Optional<User> max = Arrays
+        .asList(new User("Tony", 45, Arrays.asList("Red", "Green")),
+            new User("Mark", 21, Arrays.asList("Blue", "Green", "White")),
+            new User("Ben", 19, Arrays.asList("Red", "Green", "Yellow", "Indigo")),
+            new User("Jed", 61, Arrays.asList("Blue", "Green", "White", "Orange", "Fuscia")))
+        .stream().max(Comparator.comparing(User::getAge));
 
-    OptionalInt max2 = IntStream.of(nums2).max();
-
-    max2.ifPresent(maxVal -> System.out.println(maxVal));
+    max.ifPresent(maxVal -> System.out.println(maxVal.getFirstName()));
   }
 
   public static long aConsumer(String[] colors) {
@@ -28,7 +31,6 @@ public class App {
         .peek(color -> System.out.println("Mapped value = " + color)).count();
     return count;
   }
-
 
 
 }
