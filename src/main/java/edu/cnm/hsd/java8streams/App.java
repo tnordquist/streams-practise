89adb5e14965e9bef403ddb3e29fd9738226187a
tnordquist <toddnordquist144@gmail.com>
2021-02-15 20:40:05 -0700

    Sorting using list and Comparator.

--- src/main/java/edu/cnm/hsd/java8streams/App.java
+++ src/main/java/edu/cnm/hsd/java8streams/App.java
@@ -1,9 +1,7 @@
 package edu.cnm.hsd.java8streams;
 
 import java.util.Arrays;
-import java.util.Optional;
-import java.util.OptionalInt;
-import java.util.stream.IntStream;
+import java.util.Comparator;
 
 public class App {
   /*
@@ -12,33 +10,11 @@ public class App {
    */
 
   private static final Integer[] nums = {34, 67, 9, 12, -33};
-  private static final int[] nums2 = {34, 67, 9, 12, -3, 1001};
+  private static final Integer[] nums2 = {34, 67, 9, 12, -3, 1001};
 
   public static void main(String[] args) {
-    Optional<User> min = Arrays
-        .asList(new User("Tony", 45, Arrays.asList("Red", "Green")),
-            new User("Mark", 61, Arrays.asList("Blue", "Green", "White")),
-            new User("Adam", 61,
-                Arrays.asList("Blue", "Green", "White", "Orange", "Fuscia", "Ink")),
-            new User("Ben", 19, Arrays.asList("Blue", "Green", "White", "Orange", "Fuscia", "Ink")))
-        .stream().min((val1, val2) -> val1.getColors().size() < val2.getColors().size() ? -1
-            : val1.getColors().size() > val2.getColors().size() ? 1 : 0);
 
-    min.ifPresent(val -> System.out.println(val.getFirstName()));
-    
-    OptionalInt min2 = IntStream.of(nums2).min();
-    
-    System.out.println(min2.getAsInt());
-  }
-
-  public static long aConsumer(String[] colors) {
-    // consumer Functional Interface
-
-    long count = Arrays.asList(colors).stream().filter(color -> color.length() > 5)
-        .peek(color -> System.out.println("Filter value = " + color))
-        .map(color -> color.toUpperCase())
-        .peek(color -> System.out.println("Mapped value = " + color)).count();
-    return count;
+    Arrays.asList(nums2).stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
   }
 
 
